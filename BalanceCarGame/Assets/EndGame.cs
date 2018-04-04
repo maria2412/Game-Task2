2b0684c7f102fc8f93b741825f056e9da8fc74cf
maria2412 <36443672+maria2412@users.noreply.github.com>
2018-04-04 08:20:18 +0200

    Colliding End Game Script

---
+++ BalanceCarGame/Assets/EndGame.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement; //allows to load new scenes or restart the current one
+using UnityEngine;
+
+public class EndGame : MonoBehaviour {
+
+	void OnTriggerEnter2D()
+	{
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+	}
+}
