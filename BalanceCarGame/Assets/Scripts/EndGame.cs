49e9a13eee31d6361c3b11a8a61419391cf70937
maria2412 <36443672+maria2412@users.noreply.github.com>
2018-04-10 15:18:46 +0200

    Score Update

--- BalanceCarGame/Assets/Scripts/EndGame.cs
+++ BalanceCarGame/Assets/Scripts/EndGame.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class EndGame : MonoBehaviour {
 
+
 	void OnTriggerEnter2D(Collider2D colInfo)
 	{
 		//if the car hits the crate, or the map, the player wil lose and scene is restarted
 		if (colInfo.CompareTag ("Collidable")) {
 			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
 		}
+			
 	}
+
+
+
 }
--- BalanceCarGame/Assets/Scripts/GoalComponent.cs
+++ BalanceCarGame/Assets/Scripts/GoalComponent.cs
@@ -8,16 +8,33 @@ public class GoalComponent : MonoBehaviour {
 
 	public Text scoreText;
 	private int count=0;
+	public bool finished;
+
+	void Start()
+	{
+		finished = false;
+	}
+
+	//is called whenever the game has to be restarted
+	void Update(){
+		//when the player loses the game freezes and is restarted when the player presses the spacebar
+		if (Input.GetKey (KeyCode.Space) && finished) {
+
+			Time.timeScale = 1; // game will no longer be paused as it is 1
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+		}
+	}
 
 	void OnTriggerEnter2D(Collider2D colInfo)
 	{
 		//if the player hits the flag, the user wins the game and the game is restarted
 		if (colInfo.CompareTag("Player")) {
 			count = count + 10;
-			SetCountText ();
+			//SetCountText ();
+			Time.timeScale = 0;
+			finished = true;
 		}
 
-		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
 	}
 
 	void SetCountText()
